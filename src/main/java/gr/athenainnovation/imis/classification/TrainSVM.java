ba87089ef093217030d3290a53f933a9e4c5e2ea
nkaragiannakis <karagiannakis.nk@gmail.com>
2014-06-27 19:35:07 +0300

    check execute permissions

--- src/main/java/gr/athenainnovation/imis/classification/TrainSVM.java
+++ src/main/java/gr/athenainnovation/imis/classification/TrainSVM.java
@@ -2,6 +2,7 @@
 package gr.athenainnovation.imis.classification;
 
 import gr.athenainnovation.imis.OSMRec.OSMRec;
+import java.io.File;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -19,13 +20,29 @@ public class TrainSVM {
     public void executeTrain(String path, double confParameter, String model, boolean isLinux){
 
         String trainLine;
+        boolean isExecutable;
+        
         if(isLinux){
+            if(new File(path + "/src/main/resources/svm_multiclass_learn").canExecute()){
+                isExecutable = true;
+            }
+            else{
+                isExecutable = new File(path + "/src/main/resources/svm_multiclass_learn").setExecutable(true,false);
+            }
+            
             trainLine = path + "/src/main/resources/svm_multiclass_learn "
             + "-c " + confParameter + " "
             + path + "/target/classes/output/vectors "     
             + path + "/target/classes/output/" + model; 
         }
         else{
+            if(new File(path + "/src/main/resources/svm_multiclass_learn.exe").canExecute()){
+                isExecutable = true;
+            }
+            else{
+                isExecutable = new File(path + "/src/main/resources/svm_multiclass_learn.exe").setExecutable(true,false);
+            } 
+            
             trainLine = path + "/src/main/resources/svm_multiclass_learn.exe "
             + "-c " + confParameter + " "
             + path + "/target/classes/output/vectors "     
@@ -39,6 +56,11 @@ public class TrainSVM {
             executor.execute(commandLine);
             
         } catch (IOException ex) {
+            if(!isExecutable){
+                System.out.println("OSMRec could not grant permission to execute the svm_multiclass process.\n "
+                        + "Please set src/main/resources/svm_multiclass process execute permission and try again.");
+                System.exit(0);
+            }
             
             Logger.getLogger(OSMRec.class.getName()).log(Level.SEVERE, null, ex);
         } 
