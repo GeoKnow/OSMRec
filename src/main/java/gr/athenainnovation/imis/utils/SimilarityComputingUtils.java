b969f80b9c62fa323f9a8a708cd74475dcbee130
nkaragiannakis <karagiannakis.nk@gmail.com>
2014-07-04 20:21:41 +0300

    cosine similarities/euclidian distances

---
+++ src/main/java/gr/athenainnovation/imis/utils/SimilarityComputingUtils.java
@@ -0,0 +1,73 @@
+package gr.athenainnovation.imis.utils;
+
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+/**
+ *
+ * @author imis-nkarag
+ */
+
+public final class SimilarityComputingUtils {
+    
+    private SimilarityComputingUtils() {
+        //preventing instantiation.                   
+    }
+    
+    public static double cosineSimilarity(TreeMap<Integer,Double> vectorA, TreeMap<Integer,Double> vectorB){
+    //computes cosine similarity for the two provided vectors given as Treemaps   
+        
+        double dotProduct = 0.0;
+        double normA = 0.0;
+        double normB = 0.0;
+
+        for ( Integer id : vectorA.keySet()){
+            if(vectorB.containsKey(id)){
+                dotProduct += vectorA.get(id)*vectorB.get(id);//1 for boolean features
+                normA += vectorA.get(id)*vectorA.get(id);
+                normB += vectorB.get(id)*vectorB.get(id);
+            }
+        }
+         
+        double cosineSim =  (dotProduct / (Math.sqrt(normA) * Math.sqrt(normB)));
+        return cosineSim;
+    }
+    
+    public static double cosineSimilarityArray(ArrayList<Integer> vectorA, ArrayList<Integer> vectorB){
+    //computes cosine similarity for the two provided vectors given as ArrayLists
+        double dotProduct = 0.0;
+        double normA = 0.0;
+        double normB = 0.0;
+
+        for (int i = 0; i < vectorA.size(); i++) {
+            dotProduct += vectorA.get(i)*vectorB.get(i);
+            normA += vectorA.get(i)*vectorA.get(i);
+            normB += vectorB.get(i)*vectorB.get(i);
+        }  
+        double cosSim =  (dotProduct / (Math.sqrt(normA) * Math.sqrt(normB)));
+        return cosSim;
+    }
+
+    public static double euclidianDistance(TreeMap<Integer, Double> vectorA, TreeMap<Integer, Double> vectorB){       
+        double sum = 0.0;
+        
+        for ( Integer id : vectorA.keySet()){
+            if(vectorB.containsKey(id)){
+                double a = vectorA.get(id);
+                double b = vectorB.get(id);
+                sum = sum + (a-b)*(a-b);
+            }
+        }
+        return Math.sqrt(sum);
+    }
+    
+    public static double euclidianDistanceArray(ArrayList<Integer> vectorA, ArrayList<Integer> vectorB){       
+        double sum = 0.0;
+        for(int i=0;i<vectorA.size();i++) {
+            Integer a = vectorA.get(i);
+            Integer b = vectorB.get(i);            
+            sum = sum + (a-b)*(a-b);
+        }
+        return Math.sqrt(sum);
+    }    
+}
