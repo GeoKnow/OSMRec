b20accba4f107a38d3ea8e5901859c1993a51d56
nkaragiannakis <karagiannakis.nk@gmail.com>
2014-07-04 20:18:11 +0300

    refactored vectors to TreeMaps

--- src/main/java/gr/athenainnovation/imis/generator/ClusterVectors.java
+++ src/main/java/gr/athenainnovation/imis/generator/ClusterVectors.java
@@ -4,11 +4,12 @@ import gr.athenainnovation.imis.OSMContainer.OSMWay;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
 import java.util.Scanner;
-import java.util.Collections;
+import java.util.TreeMap;
 
 /**
  *
@@ -16,23 +17,22 @@ import java.util.Collections;
  */
 
 public class ClusterVectors {
+    
     private final List<OSMWay> wayList;
     private final String clusterSolution;
-    private static final int numberOfFeatures = 3748;
     private ArrayList<Cluster> averageClusterVectors;
+    private ArrayList<Cluster> averageClusterIndexVectors;
 
     public ClusterVectors(List<OSMWay> wayList, String clusterSolution){
-        //Map<Integer, List<
+        
         this.clusterSolution = clusterSolution;
         this.wayList = wayList;
         averageClusterVectors = new ArrayList();
-        //initialize cluster vector
-        //get cluster output file
+        averageClusterIndexVectors = new ArrayList<>();
     }   
     
     public void produceClusterVectors(){
 
-        //HashMap<Integer, Integer> clusterToInstanceID = new HashMap<>();
         HashMap<Integer, List<Integer>> clusterToInstances = new HashMap<>();
       
         try{
@@ -42,7 +42,6 @@ public class ClusterVectors {
                 Integer clusterID = input.nextInt();
 
                 if(clusterID!=-1){ //exclude non clustered instances
-
                     if(clusterToInstances.containsKey(clusterID)){
                         List<Integer> lis = clusterToInstances.get(clusterID);
                         lis.add(k);
@@ -54,32 +53,32 @@ public class ClusterVectors {
                         clusterToInstances.put(clusterID, lis);
                     }
                 }
-            k++;    
+                k++;    
             }
         }
         catch(FileNotFoundException e){
-            System.out.println("Something went wrong, producing the clustering solution. Check your input file");
+            System.out.println("Something went wrong, producing the clustering solution. Check your input file\n\n" + e);
         }
         
-        System.out.println("producing average vectors..");              
+        System.out.print("producing average vectors..");              
         
         for(Entry<Integer, List<Integer>> clusterInstancesEntry : clusterToInstances.entrySet()){
         //iterate and construct average vectors for every cluster
             
             List<Integer> clusterInstances = clusterInstancesEntry.getValue(); //get instances of current vector
-            ArrayList<Integer> averageVector = new ArrayList<>(Collections.nCopies(numberOfFeatures, 0)); //initialise average vector
+            TreeMap<Integer,Double> averageIndexVector  = new TreeMap<>();
+            
             Cluster averageClusterVector = new Cluster(clusterInstancesEntry.getKey(), clusterInstances);
             
             List<Integer> majorityClasses = new ArrayList();
             for(Integer instanceID : clusterInstances){
             //for every instance of this cluster, get instances's vectors and compute the average cluster vector    
-                ArrayList<Integer> instanceVector = wayList.get(instanceID).getVector();
-                
-                majorityClasses.addAll(wayList.get(instanceID).getClassIDs()); //we add all the classes that the instance belong 
-
-                computeAverageVector(averageVector, instanceVector);
-            }
-            
+                //ArrayList<Integer> instanceVector = wayList.get(instanceID).getVector();
+                TreeMap<Integer,Double> instanceIndexVevtor = wayList.get(instanceID).getIndexVector();
+                //we add all the classes that the instance belong 
+                majorityClasses.addAll(wayList.get(instanceID).getClassIDs()); 
+                computeAverageVector(averageIndexVector, instanceIndexVevtor);             
+            }           
             
             HashMap<Integer, Integer> frequencyMap = new HashMap<>();
             
@@ -91,63 +90,76 @@ public class ClusterVectors {
             List<DistinctiveClasses> averageClusterClassesList = new ArrayList<>();
             
             for(Entry<Integer, Integer> classIDWithFrequency : frequencyMap.entrySet()){
-                DistinctiveClasses averageClusterClassesComparator = new DistinctiveClasses(classIDWithFrequency.getKey(), classIDWithFrequency.getValue());               
+                DistinctiveClasses averageClusterClassesComparator = 
+                                new DistinctiveClasses(classIDWithFrequency.getKey(), classIDWithFrequency.getValue());               
                 averageClusterClassesList.add(averageClusterClassesComparator);
             }
-            Collections.sort(averageClusterClassesList,new DistinctiveClasses());
+            Collections.sort(averageClusterClassesList, new DistinctiveClasses());
             
-            averageClusterVector.setSortedClusterClasses(averageClusterClassesList); //these are sorted in descending order based on frequency in the cluster
-            averageClusterVector.setVector(averageVector); //average vector
+            //these are sorted in descending order based on frequency in the cluster
+            averageClusterVector.setSortedClusterClasses(averageClusterClassesList); 
+            averageClusterVector.setIndexVector(averageIndexVector);
             averageClusterVectors.add(averageClusterVector); //add instance of cluster average vector
-            
+            averageClusterIndexVectors.add(averageClusterVector);
         }
+        System.out.print(". done.\n");
     }
     
-    private void computeAverageVector(ArrayList<Integer> averageVector, ArrayList<Integer> ve){
+    private void computeAverageVector(TreeMap<Integer,Double> averageIndexVector, TreeMap<Integer, Double> ve){
         
         for(int i=0; i<1421; i++){
             //class features
-
-          if (ve.get(i).equals(1)){
-             averageVector.set(i, 1);
-          }  
+            if(!ve.containsKey(i)){  
+                averageIndexVector.put(i, 1.0);
+            }
         }
         
-        for(int i=1421; i<1425; i++){ 
+        for(int i=1421; i<1425; i++){        
         //geometry type features. 
-        //The average vector features take an integer value of the sum of the present geometry type of each instance  
-            
-            if (ve.get(i).equals(1)){
-                int increment  = averageVector.get(i) + 1;
-                averageVector.set(i, increment);
+        //The average vector features take an integer value of the sum of the present geometry type of each instance              
+            if(ve.containsKey(i)){
+                averageIndexVector.put(i, ve.get(i)+1.0);
+            }
+            else {
+                ve.put(i, 1.0);
             }
         }
         
         for(int i=1425; i<1465; i++){ 
         //rest of geometry features: rectangle, number of points, area, circle 
-        //the features here are treated as boolean values and the averaage vector takes the OR of the instance features  
-            
-            if (ve.get(i).equals(1)){
-                averageVector.set(i, 1);
+        //the features here are treated as boolean values and the averaage vector takes the OR of the instance features             
+            if(!ve.containsKey(i)){
+                averageIndexVector.put(i,1.0);
             }
         }
         
         for(int i=1465; i<3748; i++){ 
         //textual features. The average vector features take an integer value of the sum of the present textual features     
-            if (ve.get(i).equals(1)){
-                int increment  = averageVector.get(i) + 1;
-                averageVector.set(i, increment);
+            if(ve.containsKey(i)){
+                averageIndexVector.put(i, ve.get(i)+1.0);
+            }
+            else{
+                averageIndexVector.put(i,1.0);
             }
         }
-  
+       
+        if(ve.containsKey(1749)){
+            //compute mean value for this feature from all instances
+        }
+        
+        //variance feature
+        //relation feature 
     }
     
     public void setAverageClusterVectors(ArrayList<Cluster> averageClusterVectors){
         this.averageClusterVectors = averageClusterVectors;
     }
     
-    public ArrayList<Cluster> getAverageClusterVectors(){
-        return averageClusterVectors;
+    public void setIndexAverageClusterVectors(ArrayList<Cluster> averageIndexClusterVectors){
+        this.averageClusterIndexVectors = averageIndexClusterVectors;
     }
     
+    public ArrayList<Cluster> getAverageClusterVectors(){
+        return averageClusterVectors;
+    }    
 }
