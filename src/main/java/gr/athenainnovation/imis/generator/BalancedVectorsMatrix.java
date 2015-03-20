e79bdb5e24038997a349868ba4fd27f9c6a0c81b
nkaragiannakis <karagiannakis.nk@gmail.com>
2015-03-20 15:48:38 +0200

    id calculation change

--- src/main/java/gr/athenainnovation/imis/generator/BalancedVectorsMatrix.java
+++ src/main/java/gr/athenainnovation/imis/generator/BalancedVectorsMatrix.java
@@ -21,18 +21,33 @@ import java.util.TreeMap;
  */
 public class BalancedVectorsMatrix {
     
-    private static final double CLASS_FEATURE_WEIGHT = 9; 
-    private static final double GEOMETRY_TYPE_FEATURE_WEIGHT = 15.75;   
-    private static final double RECTANGLE_FEATURE_WEIGHT = 31.5;    
-    private static final double NUMBER_OF_POINTS_FEATURE_WEIGHT = 31.5;
-    private static final double AREA_FEATURE_WEIGHT = 31.5;   
-    private static final double CIRCLE_FEATURE_WEIGHT = 31.5;
-    private static final double TEXTUAL_FEATURE_WEIGHT = 7;
-    private static final double NORMALIZED_GEOMETRY_WEIGHT = 31.5;    
+//    private static final double CLASS_FEATURE_WEIGHT = 9; 
+//    private static final double GEOMETRY_TYPE_FEATURE_WEIGHT = 15.75;   
+//    private static final double RECTANGLE_FEATURE_WEIGHT = 31.5;    
+//    private static final double NUMBER_OF_POINTS_FEATURE_WEIGHT = 31.5;
+//    private static final double AREA_FEATURE_WEIGHT = 31.5;   
+//    private static final double CIRCLE_FEATURE_WEIGHT = 31.5;
+//    private static final double TEXTUAL_FEATURE_WEIGHT = 7;
+//    private static final double NORMALIZED_GEOMETRY_WEIGHT = 31.5;   
+//    private static final double NORMALIZED_MEAN_AND_VARIANCE_WEIGHT = 31.5;
+//    private static final double NORMALIZED_MEAN_WEIGHT = 31.5;
+    
+    private static final double CLASS_FEATURE_WEIGHT = 2.4494;          //sqrt(6) = 2.4494
+    private static final double GEOMETRY_TYPE_FEATURE_WEIGHT = 4.8989;   //sqrt(6)*sqrt(4) = 4.8989
+    private static final double RECTANGLE_FEATURE_WEIGHT = 4.8989;    
+    private static final double NUMBER_OF_POINTS_FEATURE_WEIGHT = 4.8989;
+    private static final double AREA_FEATURE_WEIGHT = 4.8989;   
+    private static final double CIRCLE_FEATURE_WEIGHT = 4.8989;
+    private static final double NORMALIZED_TEXTUAL_FEATURE_WEIGHT = 2;  //sqrt(4) = 2
+    private static final double NORMALIZED_GEOMETRY_WEIGHT = 4.8989;   
+    private static final double NORMALIZED_MEAN_AND_VARIANCE_WEIGHT = 4.8989;
+    private static final double NORMALIZED_RELATION_WEIGHT = 4.8989;
+    
     private final List<OSMWay> wayList;
     private final String vectorMatrixOutputFile;
     private final String readyVectorsMatrix;
     private final int columnSize;
+    private int id;
     
     public BalancedVectorsMatrix(List<OSMWay> wayList, String vectorMatrixOutputFile, int columnSize){
         this.vectorMatrixOutputFile = vectorMatrixOutputFile;
@@ -66,58 +81,132 @@ public class BalancedVectorsMatrix {
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////                
                 
         /////////// class features portion /////////////////           
-            for(int k=0; k<1421; k++){    
-                if(indexVector.containsKey(k)){                      
-                    stringBuilder.append(k).append(" ").append(indexVector.get(k)*CLASS_FEATURE_WEIGHT).append(" ");
+            for(id = 0; id < 1422; id++){    
+                if(indexVector.containsKey(id)){                      
+                    stringBuilder.append(id).append(" ").append(indexVector.get(id)*CLASS_FEATURE_WEIGHT).append(" ");
                     nonZeros++;
                 } 
             } 
                 
-        //////////// geometry type features portion /////////              
-            for(int k=1421; k<1425; k++){    
-                if(indexVector.containsKey(k)){
+        //////////// geometry type features portion /////////      
+            while(id<1426){
+                if(indexVector.containsKey(id)){
                     nonZeros++;    
-                    stringBuilder.append(k).append(" ").append(indexVector.get(k)*GEOMETRY_TYPE_FEATURE_WEIGHT).append(" ");
+                    stringBuilder.append(id).append(" ").append(indexVector.get(id)*GEOMETRY_TYPE_FEATURE_WEIGHT).append(" ");
                 }
+                id++;
             }
-        //////// rectangle feature /////////             
-            for(int k=1425; k<1426; k++){    
-                if(indexVector.containsKey(k)){
-                    nonZeros++;    
-                    stringBuilder.append(k).append(" ").append(indexVector.get(k)*RECTANGLE_FEATURE_WEIGHT).append(" ");
-                }
+        //////// rectangle feature ///////// 
+            if(indexVector.containsKey(id)){
+                nonZeros++;    
+                stringBuilder.append(id).append(" ").append(indexVector.get(id)*RECTANGLE_FEATURE_WEIGHT).append(" ");
             }
-        //////// number of points features portion /////////          
-            for(int k=1426; k<1439; k++){    
-                if(indexVector.containsKey(k)){
+            id++; //id becomes 1427
+        //////// number of points features portion /////////  
+            while(id<1440){
+                if(indexVector.containsKey(id)){
                     nonZeros++;    
-                    stringBuilder.append(k).append(" ").append(indexVector.get(k)*NUMBER_OF_POINTS_FEATURE_WEIGHT).append(" ");
+                    stringBuilder.append(id).append(" ").append(indexVector.get(id)*NUMBER_OF_POINTS_FEATURE_WEIGHT).append(" ");
                 } 
+                id++;
             }
-        //////// area features portion /////////          
-            for(int k=1439; k<1464; k++){    
-                if(indexVector.containsKey(k)){
+            //id 1440
+        //////// area features portion /////////  
+            while(id<1465){
+                if(indexVector.containsKey(id)){
                     nonZeros++;    
-                    stringBuilder.append(k).append(" ").append(indexVector.get(k)*AREA_FEATURE_WEIGHT).append(" ");
+                    stringBuilder.append(id).append(" ").append(indexVector.get(id)*AREA_FEATURE_WEIGHT).append(" ");
                 }
-            } 
-                
-        //////// circle features portion /////////          
-            for(int k=1464; k<1465; k++){    
-                if(indexVector.containsKey(k)){
-                    nonZeros++;    
-                    stringBuilder.append(k).append(" ").append(indexVector.get(k)*CIRCLE_FEATURE_WEIGHT).append(" ");
-                }
-            } 
-                
-        //////// textual features portion /////////          
-            for(int k=1465; k<3748; k++){    
-                if(indexVector.containsKey(k)){
-                    nonZeros++;    
-                    stringBuilder.append(k).append(" ").append(indexVector.get(k)*TEXTUAL_FEATURE_WEIGHT).append(" ");
-                }
+                id++;
             }
+           //id 1465     
+        //////// circle features portion /////////   
+        //    while(id<1466){
+                if(indexVector.containsKey(id)){
+                    nonZeros++;    
+                    stringBuilder.append(id).append(" ").append(indexVector.get(id)*CIRCLE_FEATURE_WEIGHT).append(" ");
+                }
+                id++;
+         //   }
                 
+            //commented out, mean and var double values
+            /*    
+            //id 1466
+            //added variance and mean features
+            if(indexVector.containsKey(id)){
+                nonZeros++;    
+                stringBuilder.append(id).append(" ").append(indexVector.get(id)*NORMALIZED_VARIANCE_WEIGHT).append(" ");
+            }
+            id++;
+            // mean feature // 
+            //id 1467
+            if(indexVector.containsKey(id)){
+                nonZeros++;    
+                stringBuilder.append(id).append(" ").append(indexVector.get(id)*NORMALIZED_MEAN_WEIGHT).append(" ");
+            }
+            id++;
+            
+                
+                
+                
+            //relations
+            while(id<1472){
+                if(indexVector.containsKey(id)){
+                    nonZeros++;
+                    stringBuilder.append(id).append(" ").append(indexVector.get(id)).append(" ");
+                } 
+                id++;
+            }
+            
+        //////// textual features portion ///////// 2280 features
+            //id 1472           
+            while(id<3752){ //3748 became 3750 after variance and mean features
+                if(indexVector.containsKey(id)){
+                    nonZeros++;    
+                    stringBuilder.append(id).append(" ").append(indexVector.get(id)*TEXTUAL_FEATURE_WEIGHT).append(" ");
+                }
+                id++;
+            }
+            */ //end - mean and var double values
+            
+                
+            //mean and var boolean intervals   
+            while(id<1526){//1466 -> 1466 + 23 + 36 +1
+                if(indexVector.containsKey(id)){
+                    nonZeros++;    
+                    stringBuilder.append(id).append(" ").append(indexVector.get(id)*NORMALIZED_MEAN_AND_VARIANCE_WEIGHT).append(" ");
+                }
+                id++;
+            }   
+            //System.out.println("balanced mean start " + 1466 + " mean end" + 1525);
+            //end of mean and var boolean intervals    
+            
+
+            //relations
+            int relEnd = id + 5;
+            while(id<relEnd){
+                if(indexVector.containsKey(id)){
+                    nonZeros++;
+                    stringBuilder.append(id).append(" ").append(indexVector.get(id)*NORMALIZED_RELATION_WEIGHT).append(" ");
+                } 
+                id++;
+            }
+            
+            
+            //textual features
+            int texEnd = relEnd + 2280;
+            while(id<texEnd){
+                if(indexVector.containsKey(id)){
+                    nonZeros++;    
+                    stringBuilder.append(id).append(" ").append(indexVector.get(id)*NORMALIZED_TEXTUAL_FEATURE_WEIGHT).append(" ");
+                }
+                id++;
+            }   
+            //System.out.println("balanced tex start(relEnd) " + relEnd + " tex end" + texEnd);          
+            
+            
+            
+            
             //if area is bigger than max permited value, make the feature 1. 
             if(node.getGeometry().getArea() < maxArea){
                 double normalizedAreaFeature = (node.getGeometry().getArea()/maxArea)*NORMALIZED_GEOMETRY_WEIGHT;                
@@ -125,8 +214,7 @@ public class BalancedVectorsMatrix {
                 nonZeros++;
             }
             else{
-                double normalizedAreaFeature = NORMALIZED_GEOMETRY_WEIGHT;
-                stringBuilder.append(columnSize+1).append(" ").append(normalizedAreaFeature).append(" ");
+                stringBuilder.append(columnSize+1).append(" ").append(NORMALIZED_GEOMETRY_WEIGHT).append(" ");
                 nonZeros++;
             }
 
