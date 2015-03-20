4e9406e8c7b61c0a662197c71e1bf32d19cd07b5
nkaragiannakis <karagiannakis.nk@gmail.com>
2015-03-20 15:49:17 +0200

    average vector alignment with vector ids change

--- src/main/java/gr/athenainnovation/imis/generator/ClusterVectors.java
+++ src/main/java/gr/athenainnovation/imis/generator/ClusterVectors.java
@@ -18,15 +18,15 @@ import java.util.TreeMap;
 
 public class ClusterVectors {
     
-    private final List<OSMWay> wayList;
+    private final List<OSMWay> trainList;
     private final String clusterSolution;
     private ArrayList<Cluster> averageClusterVectors;
     private ArrayList<Cluster> averageClusterIndexVectors;
 
-    public ClusterVectors(List<OSMWay> wayList, String clusterSolution){
+    public ClusterVectors(List<OSMWay> trainList, String clusterSolution){
         
         this.clusterSolution = clusterSolution;
-        this.wayList = wayList;
+        this.trainList = trainList;
         averageClusterVectors = new ArrayList();
         averageClusterIndexVectors = new ArrayList<>();
     }   
@@ -74,10 +74,10 @@ public class ClusterVectors {
             for(Integer instanceID : clusterInstances){
             //for every instance of this cluster, get instances's vectors and compute the average cluster vector    
                 //ArrayList<Integer> instanceVector = wayList.get(instanceID).getVector();
-                TreeMap<Integer,Double> instanceIndexVevtor = wayList.get(instanceID).getIndexVector();
+                TreeMap<Integer,Double> instanceIndexVevtor = trainList.get(instanceID).getIndexVector();
                 //we add all the classes that the instance belong 
-                majorityClasses.addAll(wayList.get(instanceID).getClassIDs()); 
-                computeAverageVector(averageIndexVector, instanceIndexVevtor);             
+                majorityClasses.addAll(trainList.get(instanceID).getClassIDs()); 
+                computeAverageVector(averageIndexVector, instanceIndexVevtor); 
             }           
             
             HashMap<Integer, Integer> frequencyMap = new HashMap<>();
@@ -105,61 +105,134 @@ public class ClusterVectors {
         System.out.print(". done.\n");
     }
     
-    private void computeAverageVector(TreeMap<Integer,Double> averageIndexVector, TreeMap<Integer, Double> ve){
+    private void computeAverageVector(TreeMap<Integer,Double> averageIndexVector, TreeMap<Integer, Double> testVector){
         
-        for(int i=0; i<1421; i++){
+        for(int i=0; i<1422; i++){
             //class features
-            if(!ve.containsKey(i)){  
+            if(testVector.containsKey(i)){  
+                averageIndexVector.put(i, 1.0); //OR between average vector and train vector
+            }
+        }
+        
+        for(int i=1422; i<1426; i++){        
+        //geometry type features. 
+        //The average vector features take an integer value of the sum of the present geometry type of each instance              
+            if(testVector.containsKey(i) && averageIndexVector.containsKey(i)){
+                averageIndexVector.put(i, averageIndexVector.get(i)+1.0);
+            }
+            else if(testVector.containsKey(i)){
                 averageIndexVector.put(i, 1.0);
             }
         }
         
-        for(int i=1421; i<1425; i++){        
-        //geometry type features. 
-        //The average vector features take an integer value of the sum of the present geometry type of each instance              
-            if(ve.containsKey(i)){
-                averageIndexVector.put(i, ve.get(i)+1.0);
+        for(int i=1426; i<1466; i++){ 
+        //rest of geometry features: rectangle, number of points, area, circle 
+        //the features here are treated as boolean values and the average vector takes the OR of the instance features             
+            if(testVector.containsKey(i)){
+                averageIndexVector.put(i,1.0);
+            }
+        }
+        
+        //the mean and variance values are the mean values of all means and variances of the instance vectors 
+        int meanID = 1466;
+        /*//double values for mean and variance
+        
+        
+        if(testVector.containsKey(meanID)){
+            if(averageIndexVector.containsKey(meanID)){
+                averageIndexVector.put(meanID,(testVector.get(meanID) + averageIndexVector.get(meanID))/2);
             }
             else {
-                ve.put(i, 1.0);
+                averageIndexVector.put(meanID, testVector.get(meanID));
             }
         }
         
-        for(int i=1425; i<1465; i++){ 
-        //rest of geometry features: rectangle, number of points, area, circle 
-        //the features here are treated as boolean values and the averaage vector takes the OR of the instance features             
-            if(!ve.containsKey(i)){
-                averageIndexVector.put(i,1.0);
+        int varianceID = 1467;
+        
+        if(testVector.containsKey(varianceID)){
+            if(averageIndexVector.containsKey(varianceID)){
+                averageIndexVector.put(varianceID,(testVector.get(varianceID) + averageIndexVector.get(varianceID))/2);
+            }
+            else {
+                averageIndexVector.put(varianceID, testVector.get(varianceID));
+            }
+        }
+        */
+        
+        //boolean mean and variance 23 + 36 = 59
+        int meanEnd = meanID+60;
+        for(int i = meanID; i<meanEnd; i++){           
+            if(testVector.containsKey(i) && averageIndexVector.containsKey(i)){
+                averageIndexVector.put(i, averageIndexVector.get(i)+1.0); //if the feature exists in testVector 
+                                                                    //the value is added to the average
+            }
+            else if(testVector.containsKey(i)){
+                averageIndexVector.put(i, 1.0); //first occurance of this feature. Put initial value in average
+            }
+        }
+        //System.out.println("mean start " + meanID + " mean end " + (meanID+59));
+        
+        //rel
+        int relEnd = meanEnd+6;
+        //System.out.println("rel start " + relStart + " rel end " + (relEnd-1));
+        for(int i = meanEnd; i<relEnd; i++){//boolean var and mean
+            if(testVector.containsKey(i) && averageIndexVector.containsKey(i)){
+                averageIndexVector.put(i, averageIndexVector.get(i)+1.0);
+            }
+            else if(testVector.containsKey(i)){
+                averageIndexVector.put(i, 1.0); //initialize relation feature in average vector
             }
         }
         
-        for(int i=1465; i<3748; i++){ 
-        //textual features. The average vector features take an integer value of the sum of the present textual features     
-            if(ve.containsKey(i)){
-                averageIndexVector.put(i, ve.get(i)+1.0);
+        //textual features
+        int texStart = relEnd;
+        int texEnd = texStart + 2280;
+        //System.out.println("tex start " + texStart + " tex end " + texEnd);
+        for(int i=texStart; i<texEnd; i++){ //boolean var and mean
+        //textual features. The average vector features take an integer value of the sum of the present textual features  
+            if(testVector.containsKey(i) && averageIndexVector.containsKey(i)){
+                averageIndexVector.put(i, averageIndexVector.get(i)+1.0);
+            }            
+            else if (testVector.containsKey(i)){
+                averageIndexVector.put(i, 1.0); //initialize average vector feature
             }
-            else{
-                averageIndexVector.put(i,1.0);
-            }
-        }
-       
-        if(ve.containsKey(1749)){
-            //compute mean value for this feature from all instances
         }
         
-        //variance feature
-        //relation feature 
+        //relations
+        /*for(int i=1468; i<1473; i++){ //double var and mean
+            if(testVector.containsKey(i)){
+                averageIndexVector.put(i, 1.0);
+            }
+        }
+        
+        for(int i=1473; i<3752; i++){ //double var and mean
+        //textual features. The average vector features take an integer value of the sum of the present textual features  
+            if(testVector.containsKey(i)){
+                averageIndexVector.put(i, testVector.get(i)+1.0);
+            }
+            //else{
+            //    averageIndexVector.put(i,1.0);
+            //}
+        }*/
+//        System.out.println("avg vec size: " + averageIndexVector.size());
+//        for(Entry<Integer, Double> la : averageIndexVector.entrySet()){
+//            System.out.println(la);
+//        }
     }
     
-    public void setAverageClusterVectors(ArrayList<Cluster> averageClusterVectors){
-        this.averageClusterVectors = averageClusterVectors;
-    }
+//    public void setAverageClusterVectors(ArrayList<Cluster> averageClusterVectors){
+//        this.averageClusterVectors = averageClusterVectors;
+//    }
     
     public void setIndexAverageClusterVectors(ArrayList<Cluster> averageIndexClusterVectors){
         this.averageClusterIndexVectors = averageIndexClusterVectors;
     }
     
-    public ArrayList<Cluster> getAverageClusterVectors(){
-        return averageClusterVectors;
-    }    
+//    public ArrayList<Cluster> getAverageClusterVectors(){
+//        return averageClusterVectors;
+//    }    
+    
+    public ArrayList<Cluster> getIndexAverageClusterVectors(){
+        return averageClusterIndexVectors;
+    }
 }
