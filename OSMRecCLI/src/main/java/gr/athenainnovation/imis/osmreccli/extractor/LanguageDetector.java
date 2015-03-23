73fac5a3d12ffeea990ee0e10da4d31d4fb07606
nkaragiannakis <karagiannakis.nk@gmail.com>
2015-03-23 15:58:20 +0200

    rename package to align with plugin package name

---
+++ OSMRecCLI/src/main/java/gr/athenainnovation/imis/osmreccli/extractor/LanguageDetector.java
@@ -0,0 +1,52 @@
+
+package gr.athenainnovation.imis.osmreccli.extractor;
+
+import com.cybozu.labs.langdetect.Detector;
+import com.cybozu.labs.langdetect.DetectorFactory;
+import com.cybozu.labs.langdetect.LangDetectException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Detects language of osm textual information
+ * 
+ * @author imis-nkarag
+ */
+
+public class LanguageDetector {
+    
+    private static LanguageDetector languageDetector = null;
+    
+    private LanguageDetector(){
+        //prevent instatiation
+    }
+    
+    public static LanguageDetector getInstance(String languageProfilesPath){
+        System.out.println("language profile path: \n" + languageProfilesPath + "/el");
+        if(languageDetector == null){
+            try {
+                languageDetector = new LanguageDetector();
+                init(languageProfilesPath);
+            } catch (LangDetectException ex) {
+                Logger.getLogger(LanguageDetector.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return languageDetector;        
+    }
+    
+    private static void init(String profileDirectory) throws LangDetectException {
+                
+        DetectorFactory.loadProfile(profileDirectory);
+    }   
+    
+    public String detect(String text) {
+        try {
+            Detector detector = DetectorFactory.create();
+            detector.append(text);
+            return detector.detect();
+        } catch (LangDetectException ex) {
+            Logger.getLogger(LanguageDetector.class.getName()).log(Level.SEVERE, null, ex);
+            return "en";
+        }
+    }       
+}
